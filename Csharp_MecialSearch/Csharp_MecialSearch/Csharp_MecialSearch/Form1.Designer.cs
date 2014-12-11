0551e6f2a69e4f4350210b31509c34bd72986233
jjf6980 <jjf6980@rit.edu>
2014-12-11 17:19:01 -0500

    project 3 upload

---
+++ Csharp_MecialSearch/Csharp_MecialSearch/Csharp_MecialSearch/Form1.Designer.cs
@@ -0,0 +1,334 @@
+﻿namespace Csharp_MecialSearch
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.groupBoxSearch = new System.Windows.Forms.GroupBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.buttonSearch = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBoxState = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxOrgName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.closeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.countyBox = new System.Windows.Forms.ComboBox();
+            this.zipBox = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.townBox = new System.Windows.Forms.ComboBox();
+            this.groupBoxSearch.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBoxSearch
+            // 
+            this.groupBoxSearch.Controls.Add(this.townBox);
+            this.groupBoxSearch.Controls.Add(this.label6);
+            this.groupBoxSearch.Controls.Add(this.zipBox);
+            this.groupBoxSearch.Controls.Add(this.countyBox);
+            this.groupBoxSearch.Controls.Add(this.comboBox1);
+            this.groupBoxSearch.Controls.Add(this.buttonClear);
+            this.groupBoxSearch.Controls.Add(this.buttonSearch);
+            this.groupBoxSearch.Controls.Add(this.label5);
+            this.groupBoxSearch.Controls.Add(this.label4);
+            this.groupBoxSearch.Controls.Add(this.comboBoxState);
+            this.groupBoxSearch.Controls.Add(this.label3);
+            this.groupBoxSearch.Controls.Add(this.textBoxOrgName);
+            this.groupBoxSearch.Controls.Add(this.label2);
+            this.groupBoxSearch.Controls.Add(this.label1);
+            this.groupBoxSearch.Location = new System.Drawing.Point(37, 82);
+            this.groupBoxSearch.Name = "groupBoxSearch";
+            this.groupBoxSearch.Size = new System.Drawing.Size(304, 352);
+            this.groupBoxSearch.TabIndex = 0;
+            this.groupBoxSearch.TabStop = false;
+            this.groupBoxSearch.Text = "Search Criteria ";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(114, 32);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 8;
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.buttonClear.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonClear.Location = new System.Drawing.Point(0, 288);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(122, 58);
+            this.buttonClear.TabIndex = 1;
+            this.buttonClear.Text = "Clear";
+            this.buttonClear.UseVisualStyleBackColor = false;
+            this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
+            // 
+            // buttonSearch
+            // 
+            this.buttonSearch.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.buttonSearch.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonSearch.Location = new System.Drawing.Point(139, 288);
+            this.buttonSearch.Name = "buttonSearch";
+            this.buttonSearch.Size = new System.Drawing.Size(165, 58);
+            this.buttonSearch.TabIndex = 1;
+            this.buttonSearch.Text = "Search";
+            this.buttonSearch.UseVisualStyleBackColor = false;
+            this.buttonSearch.Click += new System.EventHandler(this.buttonSearch_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 198);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(50, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Zip Code";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 168);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "County";
+            // 
+            // comboBoxState
+            // 
+            this.comboBoxState.FormattingEnabled = true;
+            this.comboBoxState.Items.AddRange(new object[] {
+            "AK",
+            "AL",
+            "AR",
+            "AZ",
+            "CA",
+            "CO",
+            "CT",
+            "DE",
+            "FL",
+            "GA",
+            "HI",
+            "IA",
+            "ID",
+            "IL",
+            "IN ",
+            "KS",
+            "KY",
+            "LA",
+            "MA",
+            "MD",
+            "ME",
+            "MI",
+            "MN",
+            "MO",
+            "MS",
+            "MT",
+            "NC",
+            "ND",
+            "NE",
+            "NH",
+            "NJ",
+            "NM",
+            "NV",
+            "NY",
+            "OH",
+            "OK",
+            "OR",
+            "PA",
+            "RI",
+            "SC",
+            "SD",
+            "TN",
+            "TX",
+            "UT",
+            "VA",
+            "VT",
+            "WA",
+            "WI",
+            "WV",
+            "WY"});
+            this.comboBoxState.Location = new System.Drawing.Point(44, 102);
+            this.comboBoxState.Name = "comboBoxState";
+            this.comboBoxState.Size = new System.Drawing.Size(50, 21);
+            this.comboBoxState.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 110);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "State";
+            // 
+            // textBoxOrgName
+            // 
+            this.textBoxOrgName.Location = new System.Drawing.Point(114, 67);
+            this.textBoxOrgName.Name = "textBoxOrgName";
+            this.textBoxOrgName.Size = new System.Drawing.Size(100, 20);
+            this.textBoxOrgName.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Organization Name";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Organization Type";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(364, 82);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(401, 281);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(777, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.closeToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // closeToolStripMenuItem
+            // 
+            this.closeToolStripMenuItem.Name = "closeToolStripMenuItem";
+            this.closeToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.closeToolStripMenuItem.Text = "Close";
+            this.closeToolStripMenuItem.Click += new System.EventHandler(this.closeToolStripMenuItem_Click);
+            // 
+            // countyBox
+            // 
+            this.countyBox.FormattingEnabled = true;
+            this.countyBox.Location = new System.Drawing.Point(62, 160);
+            this.countyBox.Name = "countyBox";
+            this.countyBox.Size = new System.Drawing.Size(121, 21);
+            this.countyBox.TabIndex = 9;
+            // 
+            // zipBox
+            // 
+            this.zipBox.FormattingEnabled = true;
+            this.zipBox.Location = new System.Drawing.Point(62, 190);
+            this.zipBox.Name = "zipBox";
+            this.zipBox.Size = new System.Drawing.Size(121, 21);
+            this.zipBox.TabIndex = 10;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(6, 141);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(34, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Town";
+            // 
+            // townBox
+            // 
+            this.townBox.FormattingEnabled = true;
+            this.townBox.Location = new System.Drawing.Point(62, 133);
+            this.townBox.Name = "townBox";
+            this.townBox.Size = new System.Drawing.Size(121, 21);
+            this.townBox.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(777, 472);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.groupBoxSearch);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Medical Search";
+            this.groupBoxSearch.ResumeLayout(false);
+            this.groupBoxSearch.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBoxSearch;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxOrgName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBoxState;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button buttonSearch;
+        private System.Windows.Forms.Button buttonClear;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem closeToolStripMenuItem;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox zipBox;
+        private System.Windows.Forms.ComboBox countyBox;
+        private System.Windows.Forms.ComboBox townBox;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Csharp_MecialSearch/Csharp_MecialSearch/Csharp_MecialSearch/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Net;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace Csharp_MecialSearch
+{
+    public partial class Form1 : Form
+    {
+        string url = @"http://simon.ist.rit.edu:8080/Services/resources/ESD/";
+        public Form1()
+        {
+            InitializeComponent();
+            getOrgtypes();
+
+        }
+
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            textBoxOrgName.Text = "";
+            //textBoxZipCode.Text = "";
+            
+        }
+
+        private void closeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void buttonSearch_Click(object sender, EventArgs e)
+        {
+
+            //string orgtype = comboBox1; 
+
+                string orgType = comboBox1.SelectedItem.ToString();
+                string orgName = textBoxOrgName.Text;
+                string state = comboBoxState.SelectedItem.ToString();
+                //MessageBox.Show(orgType);
+                string zip = zipBox.SelectedItem.ToString();
+                string county = countyBox.SelectedItem.ToString();
+
+                /*XmlReader xmlFile = XmlReader.Create(@"data.xml");
+                DataSet dataSet = new DataSet();
+                */
+                XmlReader xmlFile = XmlReader.Create(url+"Organizations?type="+orgType+@"&name="+orgName+@"&town="+@"&state="+state+@"&zip="+zip+@"&county="+county);
+                DataSet dataSet = new DataSet();
+                //MessageBox.Show(url+"Organizations?type="+orgType+@"&town="+@"&state="+state+@"&zip="+zip+@"&county="+county);
+                //read the xml into dataset
+                dataSet.ReadXml(xmlFile);
+
+                //Pass row table to dataGrid the datasource
+                dataGridView1.DataSource = dataSet.Tables["row"];
+
+                dataGridView1.Columns[0].Visible = false;
+                // OR
+                //dataGridView1.Columns["OrganizationID"].Visible = false;
+
+
+                xmlFile.Close();
+
+
+
+            //XmlNodeList elemList = xmldoc.GetElementsByTagName("facility");
+
+        }
+
+        private void getOrgtypes()
+        {
+            // XmlReader xmlFile = XmlReader.Create(path + "getOrgtypes");
+            // DataSet dataSet = new DataSet();
+
+            //read the xml into dataset
+            comboBox1.Items.Add("");
+            comboBox1.SelectedIndex = 0;
+            zipBox.Items.Add("");
+            zipBox.SelectedIndex = 0;
+            countyBox.Items.Add("");
+            countyBox.SelectedIndex = 0;
+            townBox.Items.Add("");
+            townBox.SelectedIndex = 0;
+            comboBoxState.SelectedIndex = 33;
+            HttpWebRequest req = (HttpWebRequest)WebRequest.Create(url + "OrgTypes");
+            HttpWebResponse res = (HttpWebResponse)req.GetResponse();
+            XmlDocument xmldoc = new XmlDocument();
+            xmldoc.Load(res.GetResponseStream());
+
+            XmlNodeList elemList = xmldoc.GetElementsByTagName("row");
+            for (int i = 0; i < elemList.Count; i++)
+            {
+
+                string[] arr = new string[1];
+                arr[0] = elemList[i].SelectSingleNode("type").InnerText;
+
+
+
+
+                comboBox1.Items.Add(arr[0]);
+                           // MessageBox.Show();
+            }
+
+        }
+    }
+}
